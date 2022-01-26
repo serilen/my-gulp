ac5f583f2b8ed607cf3d71487a709691e9cb344d
serilen <serilen29@gmail.com>
2022-01-26 17:36:32 +0200

    add filestyle in css

--- gulpfile.js
+++ gulpfile.js
@@ -25,7 +25,7 @@ import { server } from "./gulp/tasks/server.js";
 import { scss } from "./gulp/tasks/scss.js";
 import { js } from "./gulp/tasks/js.js";
 import { images } from "./gulp/tasks/images.js";
-import { otfToTtf, ttfToWoff } from "./gulp/tasks/fonts.js";
+import { otfToTtf, ttfToWoff, fontsStyle } from "./gulp/tasks/fonts.js";
 
 //Наблюдатель за изменениями в файлах
 function watcher() {
@@ -37,7 +37,7 @@ function watcher() {
 }
 
 //Последовательная обработка шрифтов
-const fonts = gulp.series(otfToTtf, ttfToWoff);
+const fonts = gulp.series(otfToTtf, ttfToWoff, fontsStyle);
 //Основные задачи
 const mainTasks = gulp.series(fonts, gulp.parallel(copy, html, scss, js, images));
 
